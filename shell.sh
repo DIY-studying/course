a96cfde6316b1d9919f37c80c2d6100ff7bec787
学了学了 <145427991+DIY-studying@users.noreply.github.com>
2024-12-03 13:03:41 +0800

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,1 @@
+#!/bin/sh ## Example: a typical script with several problems for f in  *.m3u do # '\' use to connect the next line.   grep -qi "hq.*mp3" "$f" \    && echo  "Playlist $f contains a HQ file in mp3 format" done
